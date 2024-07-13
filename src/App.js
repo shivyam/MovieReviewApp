ec0a64e8adeb14cd844ff34bc63ea7f8cdfab5d9
shivyam <144865023+shivyam@users.noreply.github.com>
2024-07-13 01:55:00 -0400

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Layout from "./components/Layout";
 import {Routes, Route} from 'react-router-dom';
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () =>{
     try{
@@ -22,6 +26,17 @@ function App() {
 
   }
 
+  const getMovieData = async (movieId) =>{
+    try{
+      const response= await api.get(`api/v1/movies/${movieId}`);
+      const singleMovie= response.data;
+      setMovies(singleMovie);
+      setReviews(singleMovie.reviews || []);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
   useEffect(()=> {
     getMovies();
   }, [])
@@ -32,6 +47,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies= {movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData= {getMovieData} movie={movies} reviews={reviews} setReviews={setReviews}/>}></Route>
 
         </Route>
       </Routes>
